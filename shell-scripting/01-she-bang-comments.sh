ef208c6af5ba5501c7825a8d1c96d34c568ed467
mohanreddy0532 <mohanreddy0532@gmail.com>
2020-02-12 20:04:10 +0530

    New Changes

--- shell-scripting/01-she-bang-comments.sh
+++ shell-scripting/01-she-bang-comments.sh
@@ -3,9 +3,9 @@
 # Any line starts with # is a comment, And those lines are ignored by all interpreters
 
 ls -latr;
-
-
+#####################################
 date;
+####################################
 uptime;
 uname -a;
 cat topics.txt

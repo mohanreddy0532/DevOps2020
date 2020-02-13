325c979026a45b66bf4f57d8d7532f566186aa59
mtallapu <mtallapu@oc3818167352.ibm.com>
2020-02-13 16:28:58 +0530

    New Changes

--- shell-scripting/04-variables.sh
+++ shell-scripting/04-variables.sh
@@ -17,16 +17,16 @@ d=abc123    # you can put a string
 
 a=10
 echo $a
-echo ${a} ## Only few situations you need this, else always use without braces.
+echo ${a}  ## Only few situations you need this, else always use without braces.
 
 echo ${a}00
 
 # Variable names have limitations.
 # Allowed values are a-z, A-Z, 0-9, & Underscore _
 # No Special characters are allowed in variable names.
-# Variable should not start with a number., Ex: 1a=10
+### Variable should not start with a number., Ex: 1a=10
 
-echo -e "Good Morning, Today date is 2020-02-12"
+echo -e "Good Morning, Today date is 2020-02-12:" `date`
 
 # We can get values dynamically by executing commands and storing those output in variables, THis is called as command substitution.
 # ex: DATE=$(date)
@@ -34,15 +34,16 @@ echo -e "Good Morning, Today date is 2020-02-12"
 
 DATE=$(date +%F)
 echo -e "Good Morning, Today date is $DATE"
+echo Today Date is below:date;
 
 # You can do some minimal arithematic operations using variable declarations.
 # ex : add=$((1+2))
 
-i=10
+i=22
 i=$(($i+2))
 echo $i
 
-# From above you need to understand that a variable once declared can be overwritten
+# From above you need to understand that a variable once declared can be **overwritten**
 # Explore read-only variables if you dont want any variable to be over-written
 
 # Some / Most times you need variables but those should not be declared inside the script
